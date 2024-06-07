9463df1acb59940066a58a9b2b08d67196ce78ab
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-06-07 15:00:32 +0300

    Field position for Worker class defined as optional

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import Position, Worker, TaskType, Task
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + (
+        "position",
+        "first_name",
+        "last_name",
+)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional Info", {"fields": ("position", )}), )
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (("Additional Info", {
+            "fields": ("position", "first_name", "last_name", )
+        }), )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_filter = ("assignees", "deadline", "priority", )
+    search_fields = ("name", "description", "task_type", "assignees", )
+
+
+admin.site.register(Position)
+admin.site.register(TaskType)
+admin.site.unregister(Group)
